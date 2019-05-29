58068e6a071c4716000740be2b00ab717fd3175e
Damian <damianwnukowski97@gmail.com>
2019-05-29 21:43:30 +0200

    Dodanie ruchu kamery, skrypt do ataku mieczem

---
+++ Assets/Scripts/Sword.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class Sword : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float damage;
+    public float attackSpeed;
+
+    float cooldownTimer = 0;
+
+    PlayerMovement player;
+
+    void Start()
+    {
+        player = GetComponent<PlayerMovement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (cooldownTimer > 0)
+        {
+            cooldownTimer -= Time.deltaTime;
+            int direction = 1;
+            if(transform.parent.transform.rotation.y == 0){
+                direction = -1;
+            }
+            transform.rotation = Quaternion.AngleAxis(direction * 90 * (((attackSpeed - cooldownTimer)/attackSpeed)), new Vector3(0, 0,1));
+        }
+        else
+        {
+            cooldownTimer = 0;
+            transform.rotation = Quaternion.AngleAxis(0, new Vector3(0, 0,1));
+
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && cooldownTimer == 0)
+        {
+            cooldownTimer = attackSpeed;
+        }
+    }
+}
